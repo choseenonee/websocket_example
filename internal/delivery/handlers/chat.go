be9d4260c360903aa07acbc5e7dc59fe9fa2dcc5
choseenonee <okaymans44@gmail.com>
2024-06-23 15:54:37 +0300

    add: 2 chat handlers

--- internal/delivery/handlers/chat.go
+++ internal/delivery/handlers/chat.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 	"strings"
 	"websockets/internal/models"
 	"websockets/internal/repository"
@@ -18,6 +19,15 @@ type ChatHandler struct {
 	chatRepository repository.ChatRepo
 }
 
+// @Summary Create chat
+// @Tags chat
+// @Accept  json
+// @Produce  json
+// @Param name query string true "Chat name"
+// @Success 200 {object} int "Successfully created chat with id"
+// @Failure 400 {object} map[string]string "Invalid input"
+// @Failure 500 {object} map[string]string "Internal server error"
+// @Router /chat [post]
 func (ch *ChatHandler) CreateChat(c *gin.Context) {
 	chatName := c.Query("name")
 
@@ -32,4 +42,65 @@ func (ch *ChatHandler) CreateChat(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, gin.H{"chat_id": chatID})
+}
+
+// @Summary Get chats by page
+// @Tags chat
+// @Accept  json
+// @Produce  json
+// @Param page query int true "Page"
+// @Success 200 {object} int "Successfully returned chats"
+// @Failure 400 {object} map[string]string "Invalid input"
+// @Failure 500 {object} map[string]string "Internal server error"
+// @Router /chat/by_page [get]
+func (ch *ChatHandler) GetChatsByPage(c *gin.Context) {
+	pageRaw := c.Query("page")
+
+	page, err := strconv.Atoi(pageRaw)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid page query"})
+		return
+	}
+
+	chats, err := ch.chatRepository.GetChatsByPage(c.Request.Context(), page)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"err": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"chats": chats})
+}
+
+// @Summary Get chat messages
+// @Tags chat
+// @Accept  json
+// @Produce  json
+// @Param chat_id query string true "Chat id"
+// @Param page query string true "Page"
+// @Success 200 {object} int "Successfully returned messages"
+// @Failure 400 {object} map[string]string "Invalid input"
+// @Failure 500 {object} map[string]string "Internal server error"
+// @Router /chat/messages [get]
+func (ch *ChatHandler) GetChatMessagesByPage(c *gin.Context) {
+	chatIDRaw := c.Query("chat_id")
+	chatID, err := strconv.Atoi(chatIDRaw)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid page query"})
+		return
+	}
+
+	pageRaw := c.Query("page")
+	page, err := strconv.Atoi(pageRaw)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid page query"})
+		return
+	}
+
+	messages, err := ch.chatRepository.GetChatMessagesByPage(c.Request.Context(), chatID, page)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"err": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"messages": messages})
 }
--- internal/delivery/server.go
+++ internal/delivery/server.go
@@ -5,12 +5,23 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/jmoiron/sqlx"
 	"github.com/prometheus/client_golang/prometheus"
+	"websockets/internal/delivery/docs"
+	"websockets/internal/delivery/middleware"
 	"websockets/internal/delivery/routers"
 	"websockets/pkg/log"
+
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func Start(logger *log.Logs, db *sqlx.DB, messagesCountMetric *prometheus.CounterVec) {
 	r := gin.Default()
+
+	docs.SwaggerInfo.BasePath = "/"
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	mdw := middleware.InitMiddleware(logger)
+	r.Use(mdw.CORSMiddleware())
 
 	routers.RegisterChatRouter(r, db)
 	routers.RegisterWebSocketRouter(r, db, logger, messagesCountMetric)
